cd7f1f08514aa1b56075276b21533924788fe405
Darylzz <cheer_bo50@hotmail.com>
2023-10-05 13:42:20 +0700

    add module and nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { TabMenuModule } from 'primeng/tabmenu';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ResearchMapComponent } from './research-map/research-map.component';
 import { MapService } from './service/map.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, ResearchMapComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, TabMenuModule, FormsModule],
   providers: [MapService],
   bootstrap: [AppComponent],
 })
--- src/app/research-map/research-map.component.ts
+++ src/app/research-map/research-map.component.ts
@@ -1,15 +1,21 @@
-import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { MapService } from '../service/map.service';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-research-map',
   templateUrl: './research-map.component.html',
   styleUrls: ['./research-map.component.css'],
 })
-export class ResearchMapComponent implements AfterViewInit {
+export class ResearchMapComponent implements OnInit {
   @ViewChild('viewMap', { static: true }) viewMap: ElementRef;
+  item: MenuItem[] = [{ label: 'Pin' }, { label: 'XY' }, { label: 'Polygon' }];
+  clickPin: any;
+  clickXY: any;
+  clickPolyline: any;
+  clickPolygon: any;
   constructor(private mapService: MapService) {}
-  ngAfterViewInit(): void {
+  ngOnInit(): void {
     this.mapService.createMap(this.viewMap.nativeElement);
   }
 }
