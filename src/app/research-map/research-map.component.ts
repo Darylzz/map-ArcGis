e9b91e4b53aff1a5105886c1450cb0229602e9e1
Darylzz <cheer_bo50@hotmail.com>
2023-10-05 14:40:48 +0700

    can search by lat long

--- src/app/research-map/research-map.component.ts
+++ src/app/research-map/research-map.component.ts
@@ -1,6 +1,9 @@
 import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { MapService } from '../service/map.service';
 import { MenuItem } from 'primeng/api';
+import Point from '@arcgis/core/geometry/Point';
+import SimpleMarkerSymbol from '@arcgis/core/symbols/SimpleMarkerSymbol';
+import Graphic from '@arcgis/core/Graphic';
 
 @Component({
   selector: 'app-research-map',
@@ -14,6 +17,9 @@ export class ResearchMapComponent implements OnInit {
   clickXY: boolean = false;
   clickPolyline: boolean = false;
   clickPolygon: boolean = false;
+  searchLat: any = null;
+  searchLong: any = null;
+
   constructor(private mapService: MapService) {}
   ngOnInit(): void {
     this.mapService.createMap(this.viewMap.nativeElement);
@@ -24,9 +30,36 @@ export class ResearchMapComponent implements OnInit {
     this.clickPolygon = false;
     this.clickPolyline = false;
     this.clickXY = false;
+    if (this.clickPin === true) {
+      this.mapService.mapView?.when(() => {
+        this.mapService.mapView?.on('click', (event) => {
+          const mapPoint = event.mapPoint;
+          const point = new Point({
+            latitude: mapPoint.latitude,
+            longitude: mapPoint.longitude,
+          });
+          const marker = new SimpleMarkerSymbol({
+            color: 'blue',
+            outline: {
+              color: 'tranparent',
+              width: 2,
+            },
+          });
+          const graphic = new Graphic({
+            geometry: point,
+            symbol: marker,
+          });
+          this.mapService.mapView?.graphics.removeAll();
+          this.mapService.mapView?.graphics.add(graphic);
+          this.mapService.mapView?.goTo(point);
+        });
+      });
+    } else {
+    }
   }
 
   onClickXY() {
+    this.mapService.mapView?.graphics.removeAll();
     this.clickPin = false;
     this.clickPolygon = false;
     this.clickPolyline = false;
@@ -34,6 +67,7 @@ export class ResearchMapComponent implements OnInit {
   }
 
   onClickPolyline() {
+    this.mapService.mapView?.graphics.removeAll();
     this.clickPin = false;
     this.clickPolygon = false;
     this.clickPolyline = true;
@@ -41,9 +75,37 @@ export class ResearchMapComponent implements OnInit {
   }
 
   onClickPolygon() {
+    this.mapService.mapView?.graphics.removeAll();
     this.clickPin = false;
     this.clickPolygon = true;
     this.clickPolyline = false;
     this.clickXY = false;
+  }
+
+  onSubmitForm() {
+    const point = new Point({
+      latitude: this.searchLat,
+      longitude: this.searchLong,
+    });
+    const marker = new SimpleMarkerSymbol({
+      color: 'red',
+      outline: {
+        color: 'tranparent',
+        width: 2,
+      },
+    });
+    const graphic = new Graphic({
+      geometry: point,
+      symbol: marker,
+    });
+    this.mapService.mapView?.graphics.removeAll();
+    this.mapService.mapView?.graphics.add(graphic);
+    this.mapService.mapView?.goTo(point);
+  }
+
+  onClearForm() {
+    this.mapService.mapView?.graphics.removeAll();
+    this.searchLat = null;
+    this.searchLong = null;
   }
 }
